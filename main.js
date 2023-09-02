6a53d106b903264e1f8fa3a51a3d6e7a093dad4a
falcon-rex22 <120151153+falcon-rex22@users.noreply.github.com>
2023-09-01 18:52:31 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+cancion = "";
+
+function preload(){
+    cancion = loadSound("creo-aurora.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(500, 400);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 500, 400);
+}
